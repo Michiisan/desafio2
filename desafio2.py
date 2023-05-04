560280908124e5e1c1c3df273e5779340a908373
Michiisan <igna2014nacho@gmail.com>
2023-05-04 11:33:13 -0300

    update

---
+++ desafio2.py
@@ -0,0 +1,21 @@
+txt = input("Ingresa un caracter ")
+#esMinuscula = txt.islower()
+#esMayuscula = txt.isupper()
+#esNumero = txt.isdigit()
+#esLetra = txt.isalpha()
+
+#print("Es minuscula", esMinuscula)
+#print("Es mayuscula", esMayuscula)
+#print("Es numero", esNumero)
+#print("Es letra", esLetra)
+# ctrol + } 
+
+if (txt.isalpha()):
+    if(txt.islower()):
+        print("Es minuscula")
+    else:
+        print("Es mayuscula")
+elif(txt.isdigit()):
+    print("Es numero")
+else:
+    print("caracter especial")
